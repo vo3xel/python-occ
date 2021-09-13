31e3596c269cc8c4bde99c1105904ceed4f6f297
vo3xel <gh@v3x.xyz>
2021-09-13 09:29:39 +0200

    dockerhub hook added and scripts updated

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 if [ $# -eq 0 ]; then
-    echo "No PythonOCC version provided"
-    exit 1
+    echo "No PythonOCC version provided - selected last available version."
+    version="7.5.1"
+else
+    version=$1
 fi
-sed -i "s/VERSION/$1/g" environment.yml
-docker build -t vo3xel/python-occ:$1 .
-sed -i "s/$1/VERSION/g" environment.yml
+
+echo "Selected PythonOCC version: $version"
+sed -i "s/VERSION/$version/g" environment.yml
+docker build -t vo3xel/python-occ:$version .
+sed -i "s/$version/VERSION/g" environment.yml
